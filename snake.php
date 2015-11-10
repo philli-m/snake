54f16a6509ab55df0ffcd895f45514b2df71777c
phillippa morland <phillippamorland@gmail.com>
2015-11-10 12:01:27 +0100

    database logging incorrectly but html ajax submitting alert

--- snake.php
+++ snake.php
@@ -5,10 +5,10 @@ $dbc = mysqli_connect('127.0.0.1', 'root', 'ServantEgg86', 'snake_wins')
 //assigning php variables to the data stored in the $POST 
   $game_id = $_POST['game_count'];
   $true_tiles = $_POST['tile_count'];
-  $gamer_id = $_POST['name'];
+  $gameer_id = $_POST['name'];
   //check database connection before querying? 
   
-  $query = "INSERT INTO tile_game_count (score, gamer_id) VALUES ('$true_tiles', $gamer_id')"; // Explain SQL Injection to me and how it can be prevented
+  $query = "INSERT INTO tile_count (true_tiles) VALUES ('$true_tiles')"; // Explain SQL Injection to me and how it can be prevented
 
   	 mysqli_query($dbc, $query)
   		or die(mysqli_error($dbc)); //gives detailed error - mysqli_query() expects parameter 1 to be mysqli, null given in 
